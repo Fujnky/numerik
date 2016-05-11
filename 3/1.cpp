31cc340f91b039dd3580f2741e7cb992fecd05e5
Lars Funke <lars.funke@udo.edu>
2016-05-11 17:01:58 +0200

    Aufg3

---
+++ 3/1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <valarray>
+
+using namespace std;
+
+//test function definition
+double f(double x) {
+  return 1 / (x*x);
+}
+
+double myQuadratur1D(double (*f)(double), valarray<double>& w, valarray<double>& p) {
+  //calculate b-a and multiplay weights
+  w *= p[p.size()-1] - p[0];
+
+  //multiply weights with function values and sum up
+  return (w * p.apply(f)).sum();
+}
+
+void myQuadratur1DTest() {
+  //define points and weights
+  valarray<double> p1 { 0.5, 1 };
+  valarray<double> w1 { 0.5, 0.5 };
+  //execute function
+  cout << myQuadratur1D(f, w1, p1) << endl;
+
+  valarray<double> p2 { 0.5, 0.75, 1 };
+  valarray<double> w2 { 1.0/6, 2.0/3, 1.0/6 };
+  cout << myQuadratur1D(f, w2, p2) << endl;
+
+  valarray<double> p3 { 0.5, 4.0/6, 5.0/6, 1 };
+  valarray<double> w3 { 1.0/8, 3.0/8, 3.0/8, 1.0/8 };
+  cout << myQuadratur1D(f, w3, p3) << endl;
+}
+
+int main() {
+  myQuadratur1DTest();
+  return 0;
+}
